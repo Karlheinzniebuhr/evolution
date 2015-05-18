ca99b78531aa7a3a9014cd15f96a37b366f19a1d
karlheinz.niebuhr <karlheinz.niebuhr@gmx.com>
2015-05-18 15:57:06 -0400

    first experiment

--- evolution.rb
+++ evolution.rb
@@ -1,40 +1,80 @@
 class Live
-	@@number = 0
-	@@alive = false
-	def initialize(genes)
-		@@number += 1
-		@@genes = genes
-		@@alive = true
+	@@population = 0
+
+	def initialize(genes=0)
+		@@population += 1
+		@genes = genes
+		@alive = true
 	end
-	def reproduce()
-		Live.new(rand(self.genes - 1..10))
+	def reproduce(genes)
+		Live.new(genes)
 	end
 	def die()
-		@@number -= 1
-		@@alive = false
+		@@population -= 1
+		@alive = false
 	end
 	def number_of_lives_alive()
-		@@number
+		@@population
 	end
-	def genes()
-		@@genes
+	def genes(gen=0)
+		@genes += gen
 	end
 	def is_alive
-		@@alive
+		@alive
+	end
+	def mutate()
+		@genes += rand(-1..1)
 	end
 end
+
+class World
+	@@iterations = 0
+	@@population = []
+	def initialize
+	end
+	def run(live, years, max_pop_length)
+		@@population << live
+		for year in 1..years
+			for cell in @@population
+				
+				if cell.genes == 5
+					new_cell = cell.reproduce(cell.genes)
+					new_cell.mutate
+					@@population << new_cell
+				else
+					cell.die
+					@@population.delete(cell)
+				end
+
+				if @@population.length > max_pop_length
+					puts "population too large"
+					return @@population
+				end
+			end
+		end
+		return @@population
+	end
+end
+
+
 
 seed = Live.new(rand(0..10))
 live = seed
-puts live
 
-for i in 2..100
-	if live.genes > 5
-		puts live.genes
-		live = live.reproduce
-	else
-		live.die
-	end
+world = World.new
+pop = []
+pop = world.run(live, 100, 1000)
+
+puts "end"
+unless pop.empty?
+	puts "They survived! Number of population is #{pop.length}"
+else
+	puts "the population didn't make it"
 end
-puts live.number_of_lives_alive
-puts live.is_alive
+puts "alive = #{live.number_of_lives_alive}"
+
+
+frequency = Hash.new(0)
+pop.each {|x| frequency[x.genes] += 1}
+frequency.sort
+frequency.each {|x,y| puts "#{x}: #{y}"}
